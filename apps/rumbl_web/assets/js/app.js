0cd787216830d789f5e86f20f8af96bfcb1ffe1e
kamalogudah <mcpaul2058@gmail.com>
2019-09-14 14:27:00 +0300

    Moving Assets and Improving tests

--- apps/rumbl_web/assets/js/app.js
+++ apps/rumbl_web/assets/js/app.js
@@ -14,4 +14,15 @@ import "phoenix_html"
 // Import local files
 //
 // Local files can be imported directly using relative paths, for example:
-// import socket from "./socket"
+import socket from "./socket";
+import Video from "./video";
+
+Video.init(socket, document.getElementById("video"))
+  // import Player from "./player";
+  // let video = document.getElementById("video");
+
+// if (video) {
+//   Player.init(video.id, video.getAttribute("data-player-id"), () => {
+//     console.log("player ready!");
+//   });
+// }
